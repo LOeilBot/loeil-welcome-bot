76257bcbd6985c7ebfb38ff95b1cca599519467a
LOeilBot <service.loeil@gmail.com>
2026-06-02 21:08:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ import discord
 from discord.ext import commands
 import os
 
-# Disable voice-related features
-discord.opus._load_default = lambda: None
-
 intents = discord.Intents.default()
 intents.message_content = True
-intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
